6c000e6566a2062c9c6e666e2a46e62f188956eb
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-02-15 22:15:19 +0500

    Env

--- config/config.py
+++ config/config.py
@@ -5,11 +5,7 @@ load_dotenv()
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_NAME = os.getenv("DB_NAME")
+    
     SUPERGROUP_ID = os.getenv("GROUP_ID")
 
 
