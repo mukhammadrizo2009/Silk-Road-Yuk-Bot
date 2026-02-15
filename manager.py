8676207d2c66d384068c82e37be8945c12b3fcea
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-02-15 07:15:44 +0500

    Register

--- manager.py
+++ manager.py
@@ -1,7 +1,24 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from config.config import config
+from apps.start import start
+from apps.
+
+Base.metadata.create_all(bind=engine)
+
 def main() -> None:
     
     
     updater = Updater(config.BOT_TOKEN)
     dispatcher = updater.dispatcher
+    
+    dispatcher.add_handler(CommandHandler('start', start))
+    
+    dispatcher.add_handler(MessageHandler(Filters.text("Shaxsingizni tasdiqlang! 🪪"), check_register))
+    dispatcher.add_handler(MessageHandler(Filters.text("Ro'yhatdan o'tganman!✅"), check_register))
+    
+    
+    updater.start_polling()
+    updater.idle()
+    
+if __name__ == "__main__":
+    main()
